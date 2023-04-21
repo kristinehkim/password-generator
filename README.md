466d3c769c96aa1664f3e24bb5b099339699a9d7
kristinehkim <122055613+kristinehkim@users.noreply.github.com>
2023-04-20 17:46:55 -0700

    updated JS and README

--- README.md
+++ README.md
@@ -4,10 +4,8 @@
 
 ## Table of Contents
 * [General Info](#general-information)
-* [Technologies Used](#technologies-used)
 * [Features](#features)
 * [Screenshots](#screenshots)
-* [Setup](#setup)
 * [Usage](#usage)
 * [Project Status](#project-status)
 * [Room for Improvement](#room-for-improvement)
@@ -17,22 +15,11 @@
 
 
 ## General Information
-- Provide general information about your project here.
-- What problem does it (intend to) solve?
-- What is the purpose of your project?
-- Why did you undertake it?
-The user will first choose the length of their password between 8 and 128 characters.  If the length is not within these limits, the user will be reminded to choose an appropriate length.  Then, the user will be presented with a series of prompts confirming whether or not they would like special characters, numbers, lowercase, and uppercase letters.  If none of these are chosen, the user will be reminded to choose at least one character.  Based on the characters the user chooses, one of each will be represented and a password will be generated.  This will create a secure password.
+The user will first choose the length of their password between 8 and 128 characters.  If the length is not within these limits, the user will be reminded to choose an appropriate length.  Then, the user will be presented with a series of prompts confirming whether or not they would like special characters, numbers, lowercase, and/or uppercase letters.  If none of these are chosen, the user will be reminded to choose at least one character.  Based on the characters the user chooses, one of each will be represented and a password will be generated.  This will create a secure password.
 <!-- You don't have to answer all the questions - just the ones relevant to your project. -->
 
 
-## Technologies Used
-- Tech 1 - version 1.0
-- Tech 2 - version 2.0
-- Tech 3 - version 3.0
-
-
 ## Features
-List the ready features here:
 - The user can create a password using special characters, numbers, lowercase, and/or uppercase letters.
 - A random password with one of each character selected will be generated for the user.
 
@@ -42,41 +29,22 @@ List the ready features here:
 <!-- If you have screenshots you'd like to share, include them here. -->
 
 
-## Setup
-What are the project requirements/dependencies? Where are they listed? A requirements.txt or a Pipfile.lock file perhaps? Where is it located?
-
-Proceed to describe how to install / setup one's local environment / get started with the project.
-
-
 ## Usage
-How does one go about using it?
-Provide various use cases and code examples here.
-The user must click on the red button "Generate Password" in order to be presented with a series of prompts.  The user will choose what characters to be included and a random password will be generated based on the criteria chosen.
-
-`write-your-code-here`
+The user must click on the red button that says "Generate Password" in order to be presented with a series of prompts.  The user will choose what characters to be included, and a random password will be generated based on the criteria chosen.
 
 
 ## Project Status
-Project is: _in progress_ / _complete_. If you are no longer working on it, provide reasons why.
+Project is: _in progress_. 
 
 
 ## Room for Improvement
-Include areas you believe need improvement / could be improved. Also add TODOs for future development.
-
 Room for improvement:
-- Improvement to be done 1
-- Improvement to be done 2
-
-To do:
-- Feature to be added 1
-- Feature to be added 2
+- A way to check if one of each character the user chooses is represented.
 
 
 ## Acknowledgements
-Give credit here.
-- This project was inspired by...
-- This project was based on [this tutorial](https://www.example.com).
 - Many thanks to my instuctor, Michael Pacheco, the TAs, Nirav Venkatesan and Julius Dorfman, and tutor, Dane Shrewsbury. 
+- Met in a study group with Oswaldo and Kevin.
 
 ## Contact
 Created by Kristine Kim.  Feel free to email me at kristinehkim@gmail.com. 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,18 @@
-// Define generatePassword and that is what is going to return the random password
-//Do your logic below 
+// Arrays of all the possible options
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", ",", "-", ".", "/", ":", ";", "<", ">", "[", "]", "_", "`", "{", "}", "|", "~", '"'];
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// Do all your code here, don't touch below code!
 function generatePassword() {
   var pot = [];
+  //Used pareInt to change the string to a number and created an if statement/conditional gate to verify that the length is between 8 and 128 characters.  If the user chooses outside of the limits, an alert will show.
   var charactersLength = parseInt(prompt("How many characters would you like your password to contain?  Please choose between 8 and 128."));
   // console.log(typeof(charactersLength)) I did this to make sure it would return a number.
   if (charactersLength < 8 || charactersLength > 128) {
     return alert("Your password must be between 8 and 128 characters.");
   }
-
+  //Confirm windows so the user can choose to include special characters, numeric characters, lowercase, and uppercase characters.
   var includeSpecialCharacters = confirm("Click OK to confirm including special characters.");
   // console.log(typeof(includeSpecialCharacters))
   var includeNumericCharacters = confirm("Click OK to confirm including numeric characters.");
@@ -21,14 +20,14 @@ function generatePassword() {
   var includeLowercaseCharacters = confirm("Click OK to confirm including lowercase characters.");
 
   var includeUppercaseCharacters = confirm("Click OK to confirm including uppercase characters.");
-
+  //Conditional gate so if the user doesn't choose any of the above, an alert will pop up reminding to choose at least one.
   if (includeSpecialCharacters === false &&
     includeNumericCharacters === false &&
     includeLowercaseCharacters === false &&
     includeUppercaseCharacters === false) {
     return alert("You must select at least one character type");
   }
-
+  //When the user selects a certain character, it will get added to the pot.
   if (includeSpecialCharacters) {
     //add special characters to the pot
     pot = pot.concat(specialCharacters);
@@ -45,41 +44,26 @@ function generatePassword() {
     //add uppercase characters to the pot
     pot = pot.concat(upperCaseLetters);
   }
-  var myArr = []
+  // This will randomly choose from the pot and strings it together.
+  var finalPass = []
   for (var i = 0; i < charactersLength; i++) {
     var index = Math.floor(Math.random() * pot.length);
     var userChoice = pot[index];
     // console.log(userChoice)
-    myArr.push(userChoice);
+    finalPass.push(userChoice);
   }
 
-  return myArr.join("");
- 
+  // if (includesSpecialCharacters === true) {
+  //   myArr[0] = ""
+  //   specialCharacters[12];
+  // }
+  
+  return finalPass.join("");
 
-  // var finalPass = " ";
-  // finalPass.concat(myArr.join);
-  // return finalPass;
-}
-// Choose randomly from the pot how many times the length of the password
 // Make sure at least one of each character is selected if statement // google check if the string has a certain character
 //  if it passes then it will return a password
 // if it doesn't pass reselect the characters from the pot again try a new password from scratch again google how to loop until a certain condition is met
 
-// var index = Math.floor(Math.random() * specialCharacters.length);
-// var userChoice = specialCharacters[index]; 
-// console.log(userChoice)
-
-// index = Math.floor(Math.random() * numbers.length);
-// userChoice = numbers[index]; 
-// console.log(userChoice)
-
-// index = Math.floor(Math.random() * lowerCaseLetters.length);
-// userChoice = lowerCaseLetters[index]; 
-// console.log(userChoice)
-
-// index = Math.floor(Math.random() * upperCaseLetters.length);
-// userChoice = upperCaseLetters[index]; 
-// console.log(userChoice)
 
 
 // Assignment Code - document.querySelector is just grabbing the button. document refers to the HTML document, query selector is how it's grabbing it, and it's grabbing the button with the id generate so this is saying our generateBtn is is the button we get from document.querySelector("#generate") - the query selector is just setting up the button to work when you click it and to assign the text that generate password gives it
